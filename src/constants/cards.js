5c94b378d5e75a5092cfa9c009a55a794a1b424e
Maxim Selyukov <selyukov.maxim@blacklight-it.ru>
2022-11-26 13:59:15 +0300

    add fonts

--- src/constants/cards.js
+++ src/constants/cards.js
@@ -7,11 +7,11 @@ export const COLUMNS = {
 };
 
 export const defaultTasks = [
-  { id: 1, title: 'Check Email', duration: '0:20h', status: COLUMNS.newTask },
+  { id: 1, title: 'Check email', duration: '0:20h', status: COLUMNS.newTask },
   { id: 2, title: 'Compare PPC agencies and make a report for Steven', duration: '3:00h', status: COLUMNS.newTask },
   { id: 3, title: 'Meeting with Amanda (PR department)', duration: '0:30h', status: COLUMNS.newTask },
   { id: 4, title: "Get Patrick's approval for homepage new design", duration: '0:20h', status: COLUMNS.newTask },
-  { id: 5, title: 'Check Email', duration: '0:20h', status: COLUMNS.scheduled },
+  { id: 5, title: 'Check email', duration: '0:20h', status: COLUMNS.scheduled },
   {
     id: 6,
     title: 'Write a blogpost "7 best strategies for SEO in 2020"',
@@ -19,7 +19,7 @@ export const defaultTasks = [
     status: COLUMNS.scheduled,
   },
   { id: 7, title: 'New Ad copies for Manamaja', duration: '2:00h', status: COLUMNS.scheduled },
-  { id: 8, title: 'Check Email', duration: '0:20h', status: COLUMNS.inProgress },
+  { id: 8, title: 'Check email', duration: '0:20h', status: COLUMNS.inProgress },
   {
     id: 9,
     title: "Record a video comment about last week's analytics report",
@@ -27,7 +27,7 @@ export const defaultTasks = [
     status: COLUMNS.inProgress,
   },
   { id: 10, title: 'Process all resumes for Content Marketer position', duration: '1:00h', status: COLUMNS.inProgress },
-  { id: 11, title: 'Check Email', duration: '0:20h', status: COLUMNS.completed },
+  { id: 11, title: 'Check email', duration: '0:20h', status: COLUMNS.completed },
   { id: 12, title: 'Weekly planning session', duration: '0:45h', status: COLUMNS.completed },
   {
     id: 13,
@@ -36,5 +36,5 @@ export const defaultTasks = [
     status: COLUMNS.completed,
   },
   { id: 14, title: 'Check FB Banner Design and give feedback', duration: '0:20h', status: COLUMNS.completed },
-  { id: 15, title: 'Check Email', duration: '0:20h', status: COLUMNS.completed },
+  { id: 15, title: 'Check email', duration: '0:20h', status: COLUMNS.completed },
 ];
